dffb067c61490555849294b57598503fdaf6d48f
Minhaz <nhminhaz@gmail.com>
2022-12-08 13:31:15 +0600

    solve register route problem

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Nur13171\FirstPackage\Http\Controllers\UserController;
  Route::get('home',[UserController::class,'home']);
 
  Route::get('register',[UserController::class,'index']);
-
+ Route::get('register',[UserController::class,'index'])->name('register');
  Route::get('login',[UserController::class,'Userlogin'])->name('user.login');
  Route::post('store',[UserController::class,'UserStore'])->name('user.store');
