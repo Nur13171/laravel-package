a1bdf645e75405b8d3f1ccac74c2bdb4b4afcbc0
Minhaz <nhminhaz@gmail.com>
2022-12-10 12:42:33 +0600

    change type

--- database/migrations/2014_10_12_000000_create_rexoitusers_table.php
+++ database/migrations/2014_10_12_000000_create_rexoitusers_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('phone');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('type');
             $table->rememberToken();
             $table->timestamps();
         });
