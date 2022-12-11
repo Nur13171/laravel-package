3def1697807fca29ea7b3e8e1415e908aea8ef92
Minhaz <nhminhaz@gmail.com>
2022-12-11 15:00:51 +0600

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Nur13171\FirstPackage\Facades\FirstPackage;
 use Nur13171\FirstPackage\Http\Controllers\UserController;
 
- Route::get('dashboard',[UserController::class,'ViewDashboard'])->name('dashboard');
+ Route::get('dashboard',[UserController::class,'ViewDashboard'])->name('dashboard')->middleware('admin');
  Route::get('register',[UserController::class,'UserRegister'])->name('user.register');
  Route::post('store',[UserController::class,'UserStore'])->name('user.store');
  Route::get('/',[UserController::class,'Index'])->name('index');
