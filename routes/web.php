df62ce21837d562a9f76b52a5fdca8c986c8570d
Minhaz <nhminhaz@gmail.com>
2022-12-11 12:05:19 +0600

    chage url

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ use Nur13171\FirstPackage\Http\Controllers\UserController;
  Route::get('dashboard',[UserController::class,'ViewDashboard'])->name('dashboard');
  Route::get('register',[UserController::class,'UserRegister'])->name('user.register');
  Route::post('store',[UserController::class,'UserStore'])->name('user.store');
- Route::get('index',[UserController::class,'Index'])->name('index');
+ Route::get('/',[UserController::class,'Index'])->name('index');
  Route::post('login',[UserController::class,'UserLogin'])->name('user.login');
  route::get('logout',[UserController::class,'Logout'])->name('logout');
